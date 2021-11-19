49b16ee925173ec89be8003cecd79b5579fde9d8
nquenoui <nathan.quenouillere@etudiant.univ-rennes1.fr>
2021-11-19 14:32:30 +0000

    first push - fin fixtures

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker;
+use Faker\Factory;
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+
+        for ($i = 1; $i <= 10; $i++)
+        {
+            $article = new Article();
+
+            $sentence = $faker->sentence(4);
+            $title = substr($sentence, 0, strlen($sentence) - 1);
+            $article->setTitle($title)
+                    ->setAuthor($faker->name())
+                    ->setContent($faker->text(3000))
+                    ->setCreatedAt($faker->dateTimeThisYear());
+
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+}
